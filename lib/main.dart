50a56117dad76d74ae82981bb197072f61087231
keider21 <79706834+keider21@users.noreply.github.com>
2025-09-28 20:04:17 +0000

    fix(main): limpiar y usar appRouter con Firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      routerConfig: router,
+      routerConfig: appRouter,
     );
   }
 }
